dca17d11db3fe8f5e685986f49def132fabf5ac7
Mikel Cabezas <mikelcabezas@gmail.com>
2023-09-16 22:31:25 +0200

    start appontments app

---
+++ appointment-schedule/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
+import { CreateAppointmentComponent } from './components/create-appointment/create-appointment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppointmentListComponent,
+    CreateAppointmentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
