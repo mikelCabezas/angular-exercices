9c321b6b2ecf6c2339413d6b93c1072f2e6a910a
Mikel Cabezas <mikelcabezas@gmail.com>
2023-09-16 13:20:01 +0200

    add bmi calculator exercise

---
+++ BMI-calculator/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ResultComponent } from './components/result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
